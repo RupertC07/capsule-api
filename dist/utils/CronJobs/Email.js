fdd7f3541243374e29c6f165dba5f91fde7c245c
Rupert Caingal <70898073+RupertC07@users.noreply.github.com>
2024-09-28 22:54:11 +0800

    Update Email.js

--- dist/utils/CronJobs/Email.js
+++ dist/utils/CronJobs/Email.js
@@ -17,7 +17,7 @@ const node_cron_1 = __importDefault(require("node-cron"));
 const EmailService_1 = __importDefault(require("../../Services/EmailService"));
 const StartEmailCron = () => __awaiter(void 0, void 0, void 0, function* () {
     node_cron_1.default.schedule("* * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
-        // console.log("*********Cron starting**********");
+        console.log("*********Cron starting**********");
         const email = new EmailService_1.default();
         const date = new Date();
         const BATCH_SIZE = 10;
@@ -30,20 +30,20 @@ const StartEmailCron = () => __awaiter(void 0, void 0, void 0, function* () {
             if (pending.length > 0) {
                 for (const record of pending) {
                     const send = yield email.send(record);
-                    //   if (send) {
-                    //     console.log(`Ref:${record.reference} has been sent`);
-                    //   } else {
-                    //     console.log(`Ref:${record.reference} failed to send`);
-                    //   }
+                      if (send) {
+                        console.log(`Ref:${record.reference} has been sent`);
+                      } else {
+                        console.log(`Ref:${record.reference} failed to send`);
+                      }
                 }
-                // console.log(`Pending emails : ${pending.length}`);
+                console.log(`Pending emails : ${pending.length}`);
             }
             else {
-                // console.log("No pending emails found");
+                console.log("No pending emails found");
             }
         }
         catch (error) {
-            //   console.log(error);
+              console.log(error);
         }
     }));
 });
